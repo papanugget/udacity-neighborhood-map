64afe9feef4e793cd88e71487b02adbb41379d61
papanugget <evilgenius04@gmail.com>
2018-10-07 13:02:09 -0400

    Initial commit

---
+++ src/components/Sidebar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { slide as Menu } from 'react-burger-menu';
+
+class Sidebar extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            
+        }
+    }
+}
+
+export default Sidebar;
