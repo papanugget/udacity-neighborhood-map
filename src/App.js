d66e8717a87be894dc10108ba15f79f984dc4f7c
papanugget <evilgenius04@gmail.com>
2018-10-07 23:31:01 -0400

    got updated props to pass to sidebar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import mapboxgl from 'mapbox-gl'
+import mapboxgl from 'mapbox-gl';
+import Sidebar from './components/Sidebar';
 import './App.css';
 
 class App extends Component {
@@ -10,12 +11,55 @@ class App extends Component {
       lng: -73.985128,
       lat: 40.758939,
       zoom: 12,
-      data: {}
+      locations: {},
+      query: ''
     }
   }
     componentDidMount() {
+      // get restoom data 
+      this.getRestrooms();   
+    }
+    // get restroom function
+    getRestrooms = () => {
+      // init Object to pass to request arg
+      const myInit = {
+        method: 'GET',
+        headers: new Headers(),
+        mode: 'cors',
+        cache: 'default'
+      };
+      // new request for JSON data from API
+      const restroomData = new Request('https://api.myjson.com/bins/d8i44', myInit);
+
+      // init empty var to contain restroomData
+      let restroomMarkers;
+
+      // fetch request
+      fetch(restroomData)
+      // promise return 
+      .then( res => {
+        // checks for ok status
+        if(res.ok) {
+          return res.json();
+        }
+        // error if response not ok 
+        throw new Error('No network response.  Please try again later.');
+      }).then( res => { 
+        // send response data to restroomMarkers obj
+        restroomMarkers = res;
+        // send data object to state
+        this.setState({locations: restroomMarkers});
+        this.mapInit(restroomMarkers);
+        // console.log(this.state.locations);
+        // log any errors
+      }).catch( err => console.log('Error: ', err));
+    }
+    
+    // load map after restroom data is retrieved
+    mapInit = restroomMarkers => {
       mapboxgl.accessToken = this.state.api_key;
       const { lng, lat, zoom } = this.state;
+      // console.log(restroomMarkers);
       const map = new mapboxgl.Map({
         container: this.mapContainer,
         style: 'mapbox://styles/papanugget/cjmmeyf99ddth2rnyoua824ey',
@@ -23,130 +67,107 @@ class App extends Component {
         zoom
       });
 
-      // init Object to pass to request arg
-      const myInit = {
-        method: 'GET',
-        headers: new Headers(),
-        mode: 'cors',
-        cache: 'default'
-
-      };
-
-      // init empty var to contain restroomData
-      let restroomMarkers;
-
-      // new request for JSON data from API
-      const restroomData = new Request('https://api.myjson.com/bins/d8i44', myInit);
-      
-      // fetch request
-      fetch(restroomData)
-        // promise return 
-        .then( res => {
-          // checks for ok status
-          if(res.ok) {
-            return res.json();
+      // load map
+      map.on('load', () => {
+        map.addLayer({
+          id: 'locations',
+          type: 'symbol',
+          source: {
+            type: 'geojson',
+            data: restroomMarkers
+          },
+          layout: {
+            'icon-image': 'custom-marker',
+            'icon-allow-overlap': true
           }
-          // error if response not ok 
-          throw new Error('No network response.  Please try again later.');
-        }).then( res => {
-          // send response data to restroomMarkers obj
-          restroomMarkers = res;
-          // send data object to state
-          this.setState({data: restroomMarkers})
-          // console.log(this.state.data);
-          // log any errors
-        }).catch( err => console.log('Error: ', err));
-
-        // load map
-        map.on('load', () => {
-          map.addLayer({
-            id: 'locations',
-            type: 'symbol',
-            source: {
-              type: 'geojson',
-              data: restroomMarkers
-            },
-            layout: {
-              'icon-image': 'custom-marker',
-              'icon-allow-overlap': true
-            }
-          });
+        });
           // function call with restroomMarkers data
           addMarkers(restroomMarkers);
         });
 
-        // change coordinates displayed when map moves
-        map.on('move', () => {
-          const { lng, lat } = map.getCenter();
-          this.setState({
-            lng: lng.toFixed(4),
-            lat: lat.toFixed(4),
-            zoom: map.getZoom().toFixed(2)
+      // change coordinates displayed when map moves
+      map.on('move', () => {
+        const { lng, lat } = map.getCenter();
+        this.setState({
+          lng: lng.toFixed(4),
+          lat: lat.toFixed(4),
+          zoom: map.getZoom().toFixed(2)
+        });
+      });
+
+      function addMarkers(data) {
+        // iterate over passed in object data
+        data.features.map((marker, index) => {
+          // create a div for each object element
+          let mapMarker = document.createElement('div');
+          // add class marker to each created div
+          mapMarker.classList = 'marker';
+          // new mapbox marker for each created marker div
+          new mapboxgl.Marker(mapMarker, {offset: [0, -20]})
+            .setLngLat(marker.geometry.coordinates)
+            .addTo(map);
+          // event listner for each map marker
+          mapMarker.addEventListener('click', (e) => {
+            // console.log(e);
+            // select any previous active markers
+            e.stopPropagation();
+            createPopup(marker);
+            flyToMarker(marker);
           });
         });
+      };
 
-        function addMarkers(data) {
-          // iterate over passed in object data
-          data.features.map((marker, index) => {
-            // create a div for each object element
-            let mapMarker = document.createElement('div');
-            // add class marker to each created div
-            mapMarker.classList = 'marker';
-            // new mapbox marker for each created marker div
-            new mapboxgl.Marker(mapMarker, {offset: [0, -20]})
-              .setLngLat(marker.geometry.coordinates)
-              .addTo(map);
-            // event listner for each map marker
-            mapMarker.addEventListener('click', (e) => {
-              console.log(e);
-              // select any previous active markers
-              e.stopPropagation();
-              createPopup(marker);
-              flyToMarker(marker);
-            });
-          });
+      function createPopup(currentMarker) {
+        let current = currentMarker.properties;
+        // select any previous popups
+        const popups = document.getElementsByClassName('mapboxgl-popup');
+        // console.log(popups);
+        // remove previous popups from display
+        if(popups[0]) {
+          popups[0].remove();
         };
+        // create popup at selected marker
+        let newPopup = new mapboxgl.Popup({ closeOnClick: false})
+          .setLngLat(currentMarker.geometry.coordinates)
+          .setHTML(`<h3>${current.name}</h3>
+                      <h4>Address: ${current.address}</h4>
+                      <div class="details">${current.year_round ? 'Open year round' : ''}</div>
+                      <div class="details">${current.handicap_a11y ? '<div id="a11y"></div>' : ''}
+            `)
+          .addTo(map);
+      };
 
-        function createPopup(currentMarker) {
-          let current = currentMarker.properties;
-          // select any previous popups
-          const popups = document.getElementsByClassName('mapboxgl-popup');
-          console.log(popups);
-          // remove previous popups from display
-          if(popups[0]) {
-            popups[0].remove();
-          };
-          // create popup at selected marker
-          let newPopup = new mapboxgl.Popup({ closeOnClick: false})
-            .setLngLat(currentMarker.geometry.coordinates)
-            .setHTML(`<h3>${current.name}</h3>
-                        <h4>Address: ${current.address}</h4>
-                        <div class="details">${current.year_round ? 'Open year round' : ''}</div>
-                        <div class="details">${current.handicap_a11y ? '<div id="a11y"></div>' : ''}
-              `)
-            .addTo(map);
-        };
+      function flyToMarker(currentMarker) {
+        map.flyTo({
+          center: currentMarker.geometry.coordinates,
+          zoom: 15
+        })
+      };
 
-        function flyToMarker(currentMarker) {
-          map.flyTo({
-            center: currentMarker.geometry.coordinates,
-            zoom: 15
-          })
-        }
-    }
+    }   
+
 
   render() {
     // contains lng, lat coordinates for the location layer in upper right
     const { lng, lat, zoom } = this.state;
+    // console.log(this.state.data);
     return (
       <main>
+
         <div className="inline-block absolute top right mt12 ml12 bg-darken75 color-white z1 py6 px12 round-full txt-s txt-bold">
-        <div>{`Longitude: ${lng} Latitude: ${lat} Zoom: ${zoom}`}</div>
+          <div>{`Longitude: ${lng} Latitude: ${lat} Zoom: ${zoom}`}</div>
         </div>
-        <main ref={el => this.mapContainer = el} className="absolute top right left bottom" />
+
+        <div ref={el => this.mapContainer = el} className="absolute top right left bottom">
+        </div>
+
+        <Sidebar 
+          locations={this.state.locations}
+          query={this.state.query}
+        />
+
       </main>
     );
   }
 }
-
 export default App;
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,11 +2,57 @@ import React, { Component } from 'react';
 import { slide as Menu } from 'react-burger-menu';
 
 class Sidebar extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            
-        }
+    // constructor(props) {
+    //     super(props);
+    // }
+    
+    componentWillReceiveProps(nextProps) {
+        let markerLocs;
+        // console.log(nextProps.locations);
+        markerLocs = nextProps.locations;
+        this.buildRestroomList(markerLocs);
+    }
+    buildRestroomList(data) {
+        console.log(data);
+        data.features.map((restroom, index) => {
+            let currentRestroom = restroom;
+            let restroomProp = currentRestroom.properties;
+            // select locations container in sidebar and append div / item for each restroom
+            let locations = document.getElementById('locations');
+            let location = `<div class='item' id=listing-${index}>
+                                <a href='#' class='title' data-value=${index}>${restroomProp.name}</a>
+                                <div class='details'>${restroomProp.address}<br>${restroomProp.year_round ? 'Open Year Round' : ''} ${restroomProp.handicap_a11y ? 'Handicap Accessible' : ''}<br>
+                                </div>   
+                            </div><br>`;
+            locations.innerHTML += location;
+        })
+    }
+    render() {
+        return (
+            <Menu>
+                <div className="locations" id="locations">
+                    <h2 className="title-large">Public Restrooms</h2>
+                    <form id="search-form">
+                        <input 
+                            // value={this.state.term}
+                            id="restroom-search" 
+                            type="text" 
+                            // onChange={ (e) =>  {
+                            //     this.setState({query: e.target.value})
+                            // }}
+                        />
+                        <br></br>
+                        {/* You are looking for: {this.nextProps.query} */}
+                        <br></br>
+                        <label htmlFor="handicap">Handicap Accessible </label>
+                        <input type="checkbox" name="handicap" id="handicap"/>
+                        <br></br>
+                        <label htmlFor="year-round">Open Year Round</label>
+                        <input type="checkbox" name="year-round" id="year-round" />
+                    </form>
+                </div>
+            </Menu>
+        )
     }
 }
 
