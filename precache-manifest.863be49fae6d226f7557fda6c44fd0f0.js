d2ec2999066883b885d124e619949685c762ed24
papanugget <evilgenius04@gmail.com>
2018-10-09 14:06:53 -0400

    Updates

--- precache-manifest.863be49fae6d226f7557fda6c44fd0f0.js
+++ precache-manifest.863be49fae6d226f7557fda6c44fd0f0.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/udacity-neighborhood-map/static/js/runtime~main.5a24e6e3.js"
   },
   {
-    "revision": "5411bb472b6362f86a99",
-    "url": "/udacity-neighborhood-map/static/js/main.5411bb47.chunk.js"
+    "revision": "f27b9c95346d79747431",
+    "url": "/udacity-neighborhood-map/static/js/main.f27b9c95.chunk.js"
   },
   {
     "revision": "49bb4bd2d1022c6abf9b",
     "url": "/udacity-neighborhood-map/static/js/1.49bb4bd2.chunk.js"
   },
   {
-    "revision": "5411bb472b6362f86a99",
+    "revision": "f27b9c95346d79747431",
     "url": "/udacity-neighborhood-map/static/css/main.2e9558f6.chunk.css"
   },
   {
-    "revision": "9caebe8d8a56f4252cccd10c4194201c",
+    "revision": "627ac21ad86f4a5e110050327b1a43c5",
     "url": "/udacity-neighborhood-map/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/udacity-neighborhood-map/precache-manifest.f5000f75f59f5fd211b6a42fa286d6cb.js"
+  "/udacity-neighborhood-map/precache-manifest.863be49fae6d226f7557fda6c44fd0f0.js"
 );
 
 workbox.clientsClaim();
