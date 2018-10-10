980ecaf6763f3fba181ef84f709740ed6705f4f9
papanugget <evilgenius04@gmail.com>
2018-10-10 14:58:22 -0400

    moved serviceworker around

--- src/startServiceWorker.js
+++ src/startServiceWorker.js
@@ -1,6 +1,6 @@
 export default function startServiceWorker() {
     if ('serviceWorker' in navigator) {
-      navigator.serviceWorker.register('/service-worker.js', {scope: '/'})
+      navigator.serviceWorker.register('/static/service-worker.js', {scope: '/static/'})
       .then(function(reg) {
         console.log('Registration succeeded!', reg);
       })
