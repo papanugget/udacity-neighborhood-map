81e75cc5022e8bc40146c49fad7d0dc15cbbc4d1
Albert Chan <evilgenius04@gmail.com>
2018-10-10 15:11:06 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,34 +1,88 @@
-/**
- * Welcome to your Workbox-powered service worker!
- *
- * You'll need to register this file in your web app and you should
- * disable HTTP caching for this file too.
- * See https://goo.gl/nhQhGp
- *
- * The rest of the code is auto-generated. Please don't update this file
- * directly; instead, make changes to your Workbox build configuration
- * and re-run your build process.
- * See https://goo.gl/2aRDsh
- */
+/* Static */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
+const CACHE_VERSION = 1;
+const STATIC_CACHE = `static-cache-v${CACHE_VERSION}`;
+const IMAGES_CACHE = `images-cache-v`;
+const OTHERS_CACHE = `others-cache-v`;
+const allCaches = [
+  STATIC_CACHE,
+  IMAGES_CACHE,
+  OTHERS_CACHE
+];
 
-importScripts(
-  "/udacity-neighborhood-map/precache-manifest.7fa6c4233eda2841eb869e4ef319b881.js"
-);
+/* Functions */
 
-workbox.clientsClaim();
+function isImageURL(url) {
+  let img_types = ["jpg", "jpeg", "png", "gif"];
+  var isImage = false;
+  for(let type of img_types) {
+    if(url.endsWith(type)) { isImage = true; break; }
+  }
+  return isImage;
+}
 
-/**
- * The workboxSW.precacheAndRoute() method efficiently caches and responds to
- * requests for URLs in the manifest.
- * See https://goo.gl/S9QRab
- */
-self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
-workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+function storeInCache(cacheName, requestClone, responseClone) {
+  return caches.open(cacheName).then(function(cache){
+    return cache.put(requestClone, responseClone)
+  });
+}
 
-workbox.routing.registerNavigationRoute("/udacity-neighborhood-map/index.html", {
-  
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+function isExternalResources(url) {
+  return url.startsWith('http');
+}
+
+/* Listeners */
+
+self.addEventListener("install", function(event){
+  event.waitUntil(
+    caches.open(STATIC_CACHE).then(function(cache){
+      console.log("Current Cache: ", STATIC_CACHE);
+      return cache.addAll([
+        "/",
+        "/index.html"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("activate", function(event){
+  event.waitUntil(
+    caches.keys().then(function(cacheNames){
+      console.log("Clearing Old Caches...");
+      Promise.all(
+        cacheNames.map(function(cacheName){
+          if(!allCaches.includes(cacheName)) {
+            console.log(`Deleting: ${cacheName}`);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener("fetch", function(event){
+  if(event.request.method === "GET") {
+    event.respondWith(
+      caches.match(event.request).then(function(result){
+        if(result) { return result; }
+        var url = new URL(event.request.url);
+        try {
+          return fetch(event.request).then(function(response){
+            // if(url.origin !== location.origin) { useCache = OTHERS_CACHE; }
+            // else { useCache = isImageURL(event.request.url) ? IMAGES_CACHE : STATIC_CACHE; }
+            let useCache = isImageURL(event.request.url) ? IMAGES_CACHE : STATIC_CACHE;
+            storeInCache(useCache, event.request.clone(), response.clone());
+            return response;
+          });
+        }
+        catch(e) {
+          console.log(e);
+        }
+      })
+    );
+  }
+  else {
+    event.respondWith( fetch(event.request) );
+  }
 });
