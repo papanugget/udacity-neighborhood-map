46d3a847bcf1eebd35710b6e6f528f1045962628
papanugget <evilgenius04@gmail.com>
2018-10-10 15:47:25 -0400

    tweaked location of sw

--- src/startServiceWorker.js
+++ src/startServiceWorker.js
@@ -1,6 +1,6 @@
 export default function startServiceWorker() {
     if ('serviceWorker' in navigator) {
-      navigator.serviceWorker.register('/tweaked.js', {scope: '/'})
+      navigator.serviceWorker.register('/udacity-neighborhood-map/tweaked.js', {scope: '/'})
       .then(function(reg) {
         console.log('Registration succeeded!', reg);
       })
